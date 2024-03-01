21e129144584a18aff6dc39d929be9a5c7b4723c
fabio <fabio@fabiopozzi.net>
2024-03-01 12:27:32 +0100

    Itera sui feed della tabella feeds ed elabora elementi

--- print_rss_feed.py
+++ print_rss_feed.py
@@ -7,13 +7,13 @@ from time import strftime
 import logging
 
 logging.basicConfig(filename='/tmp/update-feeds.log', encoding='utf-8', level=logging.DEBUG)
+connection = sqlite3.connect('prova.sqlite')
 
-def processrss(url):
+def processrss(feed_id, url, feed_title):
     """
     Download the rss feed, break it up and stuff it into the database
     """
-    c = sqlite3.connect('prova.sqlite')
-    cur = c.cursor()
+    cur = connection.cursor()
     try:
         feed = feedparser.parse(url)
         feeditemcount = len(feed["entries"])
@@ -22,14 +22,12 @@ def processrss(url):
             link = entry.get("link")
             urlhash = hashlib.md5(link.encode())
             description = entry.get("description")
-            feed_title = "bho"
             if entry.get("content"):
                 description = entry.get("content")[0]["value"]
             # TODO Fix for my timezone
             published = entry.get("published_parsed")
             published = strftime("%Y-%m-%d %H:%M:%S", published)
             date_updated = datetime.now()
-            feed_id = 1
             query = (f"INSERT INTO feed_items (title, url, urlhash, content, feed_title, published_date, last_updated, feed_id)"
                      f"VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
             cur.execute(query, (title, link, urlhash.hexdigest(), description, feed_title, published, date_updated, feed_id))
@@ -39,8 +37,18 @@ def processrss(url):
         print("Error processing feed: --------------------------------------------" + url)
         return
 
-    c.commit()
+    connection.commit()
     cur.close()
 
-if __name__ == '__main__':
-    processrss("https://feeds.feedburner.com/LifereaBlog")
+
+if __name__ == "__main__":
+
+    ID = 0
+    URL = 1
+    TITLE = 2
+    cursore = connection.cursor()
+    cursore.execute("SELECT id, feedurl, title FROM feeds")
+    for row in cursore:
+        print(f"id: {row[ID]} url: {row[URL]}, titolo: {row[TITLE]}")
+        processrss(row[0], row[1], row[2])
+    cursore.close()
