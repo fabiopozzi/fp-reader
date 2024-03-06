703261aa76e2dc1e647d9f3859ac242b97456d74
fabio <fabio@fabiopozzi.net>
2024-03-06 09:47:14 +0100

    WIP: Visualizza tutti i feed_items nella homepage

--- lettore/routes.py
+++ lettore/routes.py
@@ -1,8 +1,16 @@
+import sqlite3
 from flask import render_template
 from lettore import app
+
+connection = sqlite3.connect('prova.sqlite', check_same_thread=False)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Fabio'}
-    return render_template('index.html', title='Home', user=user)
+    cursore = connection.cursor()
+    query = (f"SELECT title, url, content, feed_title, published_date, last_updated from feed_items")
+    cursore.execute(query)
+    all_news_items = cursore.fetchall()
+    cursore.close()
+    return render_template('index.html', title='Home', news=all_news_items)
+
